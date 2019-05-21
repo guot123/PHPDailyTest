68f1fa34953fc1630a27d5ecdc74d0e69b270ffe
guot123 <2268175061@qq.com>
2019-05-21 08:49:57 +0800

    日考

---
+++ day10/Math.php
@@ -0,0 +1,15 @@
+<?php
+class Math{
+    public static function arr($arr){
+        $len = '';
+        for ($i=0; $i < $arr["$len"]; $i++) { 
+
+            if ($i) {
+                
+            }
+        }
+        return ($i);
+    }
+}
+
+?>
