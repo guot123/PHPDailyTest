c087005fdd2f7b7cc0b2120da578662a96c24c4d
guot123 <2268175061@qq.com>
2019-05-28 08:56:16 +0800

    日考

---
+++ day16/Math.php
@@ -0,0 +1,11 @@
+<?php
+class Math{
+    public static function Find($target,$array)
+    {
+        $array[]='';
+        $arr = array_key_exists($target,$array);
+        return $target;
+    }
+}
+
+?>
---
+++ day16/index.php
@@ -0,0 +1,5 @@
+<?php
+include 'Math.php';
+$arr = Math::Find(3,[1,2 ,3,9],[2,4,9,12],[4,7,10,13],[6,8,11,15]);
+echo $arr;
+?>
