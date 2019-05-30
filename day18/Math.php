a645bf464374cf127631f0a37e6153a1ad354902
guot123 <2268175061@qq.com>
2019-05-30 08:53:10 +0800

    日考

---
+++ day18/Math.php
@@ -0,0 +1,9 @@
+<?php
+class Math{
+    public static function NumberOf1($n){
+        $num = $n%2==0;
+        return $num;
+    }
+}
+
+?>
---
+++ day18/index.php
@@ -0,0 +1,6 @@
+<?php
+include 'Math.php';
+$num = Math::NumberOf1(10);
+echo $num;
+
+?>
